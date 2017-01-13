623f2e24d53928d76ed0239c6e0dbedf106f0fd6
MLabash <mo3ath_lbsh@hotmail.com>
2017-01-13 18:28:45 +0100

    AngularJS assingnment 1

---
+++ controller.js
@@ -0,0 +1,25 @@
+    var myApp = angular.module('myApp', []);
+    myApp.controller('myController', function() {
+           var vm = this;
+           vm.hobbies = [];
+           vm.addInfo = true;
+        
+           vm.addHobby = function() {
+               var newHobby = {"name" : vm.hobby, "favorite":false};
+               vm.hobbies.push(newHobby);
+               vm.hobby= "";
+            }
+           
+           vm.delete = function(index) {
+               vm.hobbies.splice(index, 1);
+           }
+           
+           vm.check = function(index) {
+              vm.hobbies[index].favorite = !vm.hobbies[index].favorite;
+           }
+           
+           vm.save= function(){
+             vm.addInfo = !vm.addInfo;
+           }
+    });
+
